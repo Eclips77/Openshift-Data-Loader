c9a7d6a7e7d8a7b475a11e8aab5f94c67e6c34d1
Eclipse <Eclips77@users.noreply.github.com>
2025-08-14 15:08:19 +0300

    Add initial data entries to create_data.sql

--- Scripts/create_data.sql
+++ Scripts/create_data.sql
@@ -3,3 +3,10 @@ CREATE TABLE IF NOT EXISTS data (
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL
 );
+
+INSERT INTO data (ID, first_name, last_name) VALUES
+(1, 'Ada', 'Lovelace'),
+(2, 'Alan', 'Turing'),
+(3, 'Grace', 'Hopper'),
+(4, 'Edsger', 'Dijkstra'),
+(5, 'Donald', 'Knuth');
